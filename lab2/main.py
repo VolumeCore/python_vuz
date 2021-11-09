8564124bc6ef28a5dd7412cb1bc96f341e104594
Roman Tepechin <volumejke@gmail.com>
2021-11-09 16:28:25 +0400

    complete functionality

---
+++ lab2/main.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import argparse
+import time
+from FileReader import FileReader
+
+from Validator import Validator
+from tqdm import tqdm
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', default='input.txt')
+    parser.add_argument('--output', default='output.txt')
+
+    return parser
+
+
+parser = createParser()
+namespace = parser.parse_args()
+print(namespace)
+
+inputPath = namespace.input
+outputPath = namespace.output
+
+file = FileReader(inputPath)
+validator = Validator(file.getData())
+print(validator.parse_invalid())
+
+f = open(outputPath, 'w')
+for i in validator.parse_valid():
+    f.write(str(i) + '\n')
